753be36a2f9b547cc3dff244fcc99b52adda0ca9
maschenk <mschenk@babbel.com>
2021-12-06 18:58:44 +0100

    add request

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const run = () => {
+const run = async () => {
   const token = core.getInput('GITHUB_TOKEN');
 
-  // const oktokit = github.getOctokit(token);
+  const oktokit = github.getOctokit(token);
   const { pull_request } = github.context.payload;
+  const { additions: totalAdditions, deletions: totalDeletions, number, owner, repo } = pull_request;
 
-  console.log(pull_request);
+  const changedFiles = await octokit.rest.pulls.listFiles({
+    owner,
+    repo,
+    number
+  });
+  console.log(totalAdditions, totalDeletions, changedFiles);
 };
 
 run();
