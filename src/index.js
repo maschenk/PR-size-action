2c9b7f15636826ef6e35380164a0d1f13142580a
maschenk <mschenk@babbel.com>
2021-12-06 19:01:01 +0100

    fix typo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ const github = require('@actions/github');
 const run = async () => {
   const token = core.getInput('GITHUB_TOKEN');
 
-  const oktokit = github.getOctokit(token);
+  const octokit = github.getOctokit(token);
   const { pull_request } = github.context.payload;
   const { additions: totalAdditions, deletions: totalDeletions, number, owner, repo } = pull_request;
 
   const changedFiles = await octokit.rest.pulls.listFiles({
-    owner,
-    repo,
-    number
+    owner, repo, number
   });
   console.log(totalAdditions, totalDeletions, changedFiles);
 };
