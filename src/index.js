be801a4168ea44f782674321c27383d69f7b3464
maschenk <mschenk@babbel.com>
2021-12-07 14:38:52 +0100

    test with regex

--- src/index.js
+++ src/index.js
@@ -3,21 +3,31 @@ const github = require('@actions/github');
 
 const run = async () => {
   const token = core.getInput('GITHUB_TOKEN');
+  const ignoredFiles = JSON.parse(core.getInput('ignore_files'));
+  const expr = new RegExp(`/[a-zA-Z0-9]*${ignoredFiles}/g`);
 
   const octokit = github.getOctokit(token);
   const { pull_request, repository, number } = github.context.payload;
   const { additions: totalAdditions, deletions: totalDeletions } = pull_request;
-  console.log(repository.owner.login, repository.name, number);
-  let changedFiles;
+  let excludedAdditions, excludedDeletions;
+
   try {
-    changedFiles = await octokit.rest.pulls.listFiles({
+    const { data } = await octokit.rest.pulls.listFiles({
       owner: repository.owner.login, repo: repository.name, pull_number: number
     });
+    
+    data.forEach((file) => {
+      if(expr.test(file.filename)) {
+        excludedAdditions += file.additions;
+        excludedDeletions += file.deletions;
+      }
+    })
+
   } catch(error) {
     console.log(error.message);
   } 
 
-  console.log(totalAdditions, totalDeletions, changedFiles);
+  console.log(totalAdditions, totalDeletions, excludedAdditions, excludedDeletions);
 };
 
 run();
