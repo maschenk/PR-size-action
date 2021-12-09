0278ad3438f040da27e4c0b3785f34bbfdaac8c8
maschenk <mschenk@babbel.com>
2021-12-09 12:14:20 +0100

    try full version

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const thresholdOptions = {
+  sizeSThreshold: 10,
+  sizeMThreshold: 30,
+  sizeLThreshold: 100,
+  sizeXLThreshold: 500,
+  sizeXXLThreshold: 1000
+};
+
+export const labelOptions = {
+  sizeXSLabel: 'size/XS',
+  sizeSLabel: 'size/S',
+  sizeMLabel: 'size/M',
+  sizeLLabel: 'size/L',
+  sizeXLLabel: 'size/XL',
+  sizeXXLLabel: 'size/XXL'
+};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,74 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
+import { thresholdOptions, labelOptions } from './constants';
+
+const token = core.getInput('GITHUB_TOKEN');
+const ignoredFiles = core.getInput('ignore_files');
+const octokit = github.getOctokit(token);
 
 const run = async () => {
-  const token = core.getInput('GITHUB_TOKEN');
-  const ignoredFiles = core.getInput('ignore_files');
-  const expr = new RegExp(`[a-zA-Z0-9]*${ignoredFiles}`);
-  console.log(expr);
-  const octokit = github.getOctokit(token);
   const { pull_request, repository, number } = github.context.payload;
-  const { additions: totalAdditions, deletions: totalDeletions } = pull_request;
-  let excludedAdditions = 0;
-  let excludedDeletions = 0;
 
-  try {
-    const { data } = await octokit.rest.pulls.listFiles({
-      owner: repository.owner.login, repo: repository.name, pull_number: number
-    });
-    
-    data.forEach((file) => {
-      console.log(file.filename, file.additions, file.deletions);
-      console.log(expr.test(file.filename));
-      if(expr.test(file.filename)) {
-        excludedAdditions += file.additions;
-        excludedDeletions += file.deletions;
-      }
-    })
+  const { changes: totalChanges } = pull_request;
+  const cleanedChanges = totalChanges - getExcludedChanges(repository, number);
+  const currentLabels = getCurrentLabels(pull_request.labels);
+  const desiredLabel = getDesiredLabel(cleanedChanges);
+  const newLabel = currentLabels.includes(desiredLabel) ? '' : desiredLabel;
+  const staleLabels = currentLabels.filter(label => label !== desiredLabel);
 
-  } catch(error) {
-    console.log(error.message);
-  } 
+  core.setOutput('new_label', newLabel);
+  core.setOutput('stale_label', staleLabels);
+  };
 
-  console.log(totalAdditions, totalDeletions, excludedAdditions, excludedDeletions);
+const getExcludedChanges = (repository, number) => {
+  let excludedChanges = 0;
+  if (ignoredFiles.length > 0) {
+    const expr = new RegExp(`[a-zA-Z0-9]*${ignoredFiles}`);
+    try {
+      const { data } = await octokit.rest.pulls.listFiles({
+        owner: repository.owner.login, repo: repository.name, pull_number: number
+      });
+      
+      data.forEach((file) => {
+        console.log(file.filename, file.changes);
+        console.log(expr.test(file.filename));
+        if(expr.test(file.filename)) {
+          excludedChanges += file.changes;
+        }
+      });
+    } catch(error) {
+      console.log(error.message);
+    };
+  };
+  return excludedChanges;
 };
+
+// getDesiredLabel
+const getDesiredLabel = (cleanedChanges) => {
+  if (cleanedChanges > thresholdOptions.sizeSThreshold) {
+    return labelOptions.sizeXSLabel;
+  }
+  if (cleanedChanges > thresholdOptions.sizeMThreshold) {
+    return labelOptions.sizeSLabel;
+  }
+  if (cleanedChanges > thresholdOptions.sizeLThreshold) {
+    return labelOptions.sizeMLabel;
+  }
+  if (cleanedChanges > thresholdOptions.sizeXLThreshold) {
+    return labelOptions.sizeLLabel;
+  }
+  if (cleanedChanges > thresholdOptions.sizeXXLThreshold) {
+    return labelOptions.sizeXLLabel;
+  }
+  return labelOptions.sizeXXLLabel;
+};
+
+// getCurrentLabels
+const getCurrentLabels = (labels) => {
+  labels.filter((label) => {
+    Object.values(labelOptions).includes(label.name).map((label) => label.name);
+  })
+};
+
 
 run();
